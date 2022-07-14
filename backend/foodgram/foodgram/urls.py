9d36b4279ff7e9dbffa274ce054064b41dda18b3
eterskikh <eterskikh@nvidia.com>
2022-07-15 01:40:01 +0400

    qweqwe

--- backend/foodgram/foodgram/urls.py
+++ backend/foodgram/foodgram/urls.py
@@ -17,7 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import TemplateView
 
-
 urlpatterns = [
     path('api/', include('api.urls')),
     path('api/', include('users.urls', namespace='users')),
--- backend/foodgram/recipes/models.py
+++ backend/foodgram/recipes/models.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import get_user_model
+from django.core.validators import MinValueValidator
 from django.db import models
 from django.db.models.constraints import UniqueConstraint
-from django.core.validators import MinValueValidator
 
 User = get_user_model()
 
