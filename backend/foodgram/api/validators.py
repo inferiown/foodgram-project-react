f1fe98cd73ec179b08d9df1646608bf089f99fce
eterskikh <eterskikh@nvidia.com>
2022-07-17 19:01:15 +0400

    added validation

--- backend/foodgram/api/validators.py
+++ backend/foodgram/api/validators.py
@@ -5,10 +5,10 @@ from recipes.models import Ingredient
 def ingredients_validator(ingredient_list):
     testing_list = []
     for i in ingredient_list:
-        if i in testing_list:
+        if i['id'] in testing_list:
             failed_ingr = Ingredient.objects.get(pk=i['id'])
             raise ValidationError(
                     'Нельзя добавлять несколько '
                     f'одинаковых ингредиентов {failed_ingr}'
                     )
-        testing_list.append(i)
+        testing_list.append(i['id'])
